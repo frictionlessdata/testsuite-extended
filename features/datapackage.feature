25ad211c4d8ed0129dbd894047441420e7617240
roll <roll@post.agency>
2016-03-28 11:25:50 +0300

    implemented datapackage-sql feature test

---
+++ features/datapackage.feature
@@ -0,0 +1,9 @@
+Feature: Datapackage core and integrations work
+
+  Scenario Outline: Push/pull datapackage to SQL storage
+    When We push/pull datapackage from "<path>" to SQL
+    Then No errors are occured
+
+    Examples: Synthetic
+        | path                       |
+        | datasets/datapackages/testing |
--- features/steps/sql.py
+++ features/steps/sql.py
@@ -7,15 +7,18 @@ from __future__ import unicode_literals
 import uuid
 import sqlalchemy as sa
 from behave import given, when, then
+from datapackage import push_datapackage, pull_datapackage
 from jsontableschema import push_resource, pull_resource
 from jsontableschema.plugins.sql import Storage
-
-# Prepare sql
-engine = sa.create_engine('sqlite:///:memory:')
 
 
 @when('We push/pull resource from "{path}" to SQL')
 def step_when_push_pull_resource_to_sql(context, path):
+
+    # Prepare sql
+    # TODO: move to module-level:
+    # https://github.com/frictionlessdata/jsontableschema-sql-py/issues/21
+    engine = sa.create_engine('sqlite:///:memory:')
 
     # Generate prefix
     prefix = '%s_' % uuid.uuid4().hex
@@ -28,7 +31,8 @@ def step_when_push_pull_resource_to_sql(context, path):
             schema='%s/schema.json' % path,
             data='%s/data.csv' % path,
             backend='sql',
-            engine=engine)
+            engine=engine,
+            prefix=prefix)
 
         # Pull resource from storage
         pull_resource(
@@ -36,11 +40,58 @@ def step_when_push_pull_resource_to_sql(context, path):
             schema='target/sql/%s/schema.json' % path,
             data='target/sql/%s/data.csv' % path,
             backend='sql',
-            engine=engine)
+            engine=engine,
+            prefix=prefix)
 
     finally:
 
         # Delete test tables from storage
         storage = Storage(engine, prefix=prefix)
         for table in storage.tables:
-            storage.delete(table)
+            # TODO: remove try-except
+            # https://github.com/frictionlessdata/jsontableschema-sql-py/issues/21
+            try:
+                storage.delete(table)
+            except Exception:
+                pass
+
+
+@when('We push/pull datapackage from "{path}" to SQL')
+def step_when_push_pull_datapackage_to_sql(context, path):
+
+    # Prepare sql
+    # TODO: move to module-level:
+    # https://github.com/frictionlessdata/jsontableschema-sql-py/issues/21
+    engine = sa.create_engine('sqlite:///:memory:')
+
+    # Generate prefix
+    prefix = '%s_' % uuid.uuid4().hex
+
+    try:
+
+        # Push datapackage to storage
+        push_datapackage(
+            descriptor='%s/datapackage.json' % path,
+            backend='sql',
+            engine=engine,
+            prefix=prefix)
+
+        # Pull datapackage from storage
+        pull_datapackage(
+            descriptor='target/sql/%s/datapackage.json' % path,
+            name='name',
+            backend='sql',
+            engine=engine,
+            prefix=prefix)
+
+    finally:
+
+        # Delete test tables from storage
+        storage = Storage(engine, prefix=prefix)
+        for table in storage.tables:
+            # TODO: remove try-except
+            # https://github.com/frictionlessdata/jsontableschema-sql-py/issues/21
+            try:
+                storage.delete(table)
+            except Exception:
+                pass
